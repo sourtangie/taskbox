e99fe532b2d20fe4acad4799c320a68ba493e04c
Casper <s>
2020-09-21 13:09:42 +0200

    Initial commit

---
+++ storybook-static/precache-manifest.65e03b64957630ebad1cfb213f185ef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b666e66190bd8906ca1deabdb7e4f7da",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.caea8c87e268f80e57af.bundle.js"
+  },
+  {
+    "url": "runtime~main.caea8c87e268f80e57af.bundle.js"
+  },
+  {
+    "revision": "68a218f5519fa532c3fc",
+    "url": "static/css/main.281f400c.chunk.css"
+  },
+  {
+    "url": "vendors~main.caea8c87e268f80e57af.bundle.js"
+  },
+  {
+    "url": "vendors~main.caea8c87e268f80e57af.bundle.js.LICENSE.txt"
+  }
+]);
