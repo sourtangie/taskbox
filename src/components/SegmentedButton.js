a1366a4e914ad68ff1780c4daaebee0b86e72511
Casper <s>
2020-09-24 13:10:56 +0200

    add SegmentedButton.js to git

---
+++ src/components/SegmentedButton.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { ButtonContainer, InnerButton } from './style';
+import PropTypes from 'prop-types';
+
+
+export function SegmentedButton({ className, value, options, onChange, large}) {
+    return (
+        <ButtonContainer className={className}>
+            {options.map((option, index) => (
+                <InnerButton large={large} className ={className} key={index} active={value === option} onClick={() => onChange(option)}>
+                    {option}
+                </InnerButton>
+            ))}
+        </ButtonContainer>
+    );
+}
+SegmentedButton.propTypes = {
+    className: PropTypes.string,
+    value: PropTypes.node,
+    options: PropTypes.node,
+    onChange: PropTypes.node,
+    large: PropTypes.bool,
+};
